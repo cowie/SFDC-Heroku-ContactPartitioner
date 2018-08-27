c4fa085ac4c7c3a6c8c134882928f7179c18a8fa
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2018-08-27 12:18:40 -0700

    what do you mean i need to tell it where to connect

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL
 });
 
 pool.on('error', (err, client) => {
@@ -20,16 +20,17 @@ router.get('/getAccount', (req, res, next) => {
   // query against pg, get account details
   pool.connect((err, client, done) => {
     if (err) throw err;
-    client.query('SELECT username FROM account WHERE "accountNumber"  = $1', [1], (qerr, qres) => {
-      done();
-      if (qerr) {
-        console.log(qerr.stack);
-        res.send('something borked');
-      } else{
-        console.log(qres.rows[0]);
-        res.send('something not so borked');
-      }
-    });
+    client.query('SELECT "customerSegment", "username", "customerType", "onlineRegistered", "trialCustomer", "status", "email", "phone", "amountDue", "lastPaymentAmount", "lastPaymentDate", "billingStreet", "billingCity", "billingState", "billingPostalCode", "serviceStreet", "serviceCity", "serviceState", "servicePostalCode",  "primaryContact" FROM account WHERE "accountNumber"  = $1', 
+      [accountID], (qerr, qres) => {
+        done();
+        if (qerr) {
+          console.log(qerr.stack);
+          res.send(qerr.stack);
+        } else {
+          console.log(qres.rows[0]);
+          res.send(qres.rows[0]);
+        }
+      });
   });
   // create json payload, send back
   // close
