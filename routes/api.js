996f300714f70d0e4ca698ca0dc169b72b3889fe
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2018-08-27 11:59:39 -0700

    what do you mean i need to tell it where to connect

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
 
 pool.on('error', (err, client) => {
   console.error('Unexpected error on idle client', err);
