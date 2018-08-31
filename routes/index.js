27af3d4b0a6da0b99c2ad0caf2e1efcb1aa7cbf1
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2018-08-31 09:58:35 -0700

    building deploy automation

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const fs = require('fs');
-
+const path = require('path');
 /* GET home page. */
 router.get('/', (req, res, next) => {
   fs.readFile(path.join(process.cwd(), 'Part-Account.json'), (err, data) => {
